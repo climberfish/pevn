c0ee3f3e552151483ef8344d7fa34770a7695504
climberfishtech <climberfish.tech@gmail.com>
2021-02-09 02:18:10 -0300

    nginx network mode

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,5 +1,5 @@
 import app from 'application/app';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
