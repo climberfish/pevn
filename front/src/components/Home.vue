41db2562f2bceb60a3a86af12222b7e0c74287b9
felipe-tomino <felipe.tomino@gmail.com>
2021-03-31 22:03:56 -0300

    new front

---
+++ front/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: { msg: String },
+});
+</script>
--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,5 +1,12 @@
 import { createStore } from 'vuex';
 
 export default createStore({
-  modules: {},
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
 });
